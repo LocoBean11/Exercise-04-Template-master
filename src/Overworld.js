3ef083b6a98e9e600e086aef146af7b71d276306
Aaron Rodriguez <aatrodri@ucsc.edu>
2023-12-11 13:06:56 -0800

    Removed desktop.ini file

--- src/Overworld.js
+++ src/Overworld.js
@@ -11,7 +11,7 @@ class Overworld extends Phaser.Scene {
         })
 
         this.load.image('tilesetImage', 'tileset.png');
-        this.load.tilemapTiledJSON('tilemapJSON', 'Overworld.json');
+        this.load.tilemapTiledJSON('tilemapJSON', 'overworld.json');
     }
 
     create() {
